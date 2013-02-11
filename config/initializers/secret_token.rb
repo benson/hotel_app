4b24f75c89c283096e78243cdc4ebd2ed2edf272
BENSON <benson@ubuntu.(none)>
2013-02-11 18:22:28 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelApp::Application.config.secret_token = '886d1684361274a24b9e2d034f462b3a6b8e24bff9a90d6ca6fd359fd0a1eff3871972f18b7c67322550e81a1fd050f7189c020a0405a8d3887d8e3a413d5b0a'
